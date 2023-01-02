9fca4c2d8b914ab5da1e8b55f390efc73869d026
Yan Dias <yansantos0712@gmail.com>
2023-01-02 15:35:01 -0300

    feat:  routes adjusts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,31 @@
 import { Routes as Switch, Route } from 'react-router-dom';
-import Layout from '@/components/Layout';
-import { routes } from './routes';
-import PageNotFound from '@/pages/404';
+import { lazy } from 'react';
+
+const Layout = lazy(() => import('@/components/Layout'));
+const RegisterCard = lazy(() => import('@/pages/RegisterCard'));
+const NotFound = lazy(() => import('@/pages/404'));
+const NegociationInstallments = lazy(
+  () => import('@/pages/NegociationInstallments'),
+);
+const PaymentFirstInstallment = lazy(
+  () => import('@/pages/PaymentFirstInstallment'),
+);
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" element={<Layout />}>
-        {routes.map(route =>
-          route.isIndex ? (
-            <Route key={route.path} index element={<route.element />} />
-          ) : (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.element />}
-            />
-          ),
-        )}
-        <Route path="*" element={<PageNotFound />} />
+        <Route index element={<NegociationInstallments />} />
+        <Route
+          path="/payment-installment"
+          element={<PaymentFirstInstallment />}
+        />
+        <Route
+          path="/payment-installment"
+          element={<PaymentFirstInstallment />}
+        />
+        <Route path="/register-card" element={<RegisterCard />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Switch>
   );
